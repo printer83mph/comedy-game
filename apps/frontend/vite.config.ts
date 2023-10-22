bda00501f08669b56299a00750216d24bf646af2
Thomas Shaw <printer.83mph@gmail.com>
2023-10-21 21:36:15 -0400

    Remove unnecessary proxy

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,18 +1,10 @@
 import react from '@vitejs/plugin-react-swc';
-import { defineConfig, loadEnv } from 'vite';
-
-const env = loadEnv('all', process.cwd());
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-    proxy: {
-      '/api': {
-        target: `${env.VITE_BACKEND_URL}`,
-        changeOrigin: true,
-      },
-    },
   },
 });
